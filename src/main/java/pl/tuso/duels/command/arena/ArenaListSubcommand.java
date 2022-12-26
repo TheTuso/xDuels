b3c1126b938e4330efe784b0a9e3bd433c69f104
TheTuso <piotrekpasztor@gmail.com>
2022-12-27 00:51:32 +0100

    Added missing arena list subcommand

--- src/main/java/pl/tuso/duels/command/arena/ArenaListSubcommand.java
+++ src/main/java/pl/tuso/duels/command/arena/ArenaListSubcommand.java
@@ -2,7 +2,11 @@ package pl.tuso.duels.command.arena;
 
 import io.papermc.paper.command.PaperSubcommand;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 import pl.tuso.duels.Duels;
+import pl.tuso.duels.api.Arena;
+
+import java.util.List;
 
 public class ArenaListSubcommand implements PaperSubcommand {
     private final Duels duels;
@@ -12,7 +16,10 @@ public class ArenaListSubcommand implements PaperSubcommand {
     }
 
     @Override
-    public boolean execute(CommandSender sender, String subCommand, String[] args) {
-        return false;
+    public boolean execute(@NotNull CommandSender sender, String subCommand, String[] args) {
+        final List<Arena> arenas = this.duels.getGameSystem().getArenaManager().getArenas();
+        sender.sendMessage(this.duels.getMessages().getLine("command.arena.list.title", arenas.size()));
+        arenas.forEach(arena -> sender.sendMessage(this.duels.getMessages().getLine("command.arena.list.element", arena.getSerializedName())));
+        return true;
     }
 }
